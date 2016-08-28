b9af8e8b83ce87c15c2e640d1b1c716ca3e6eacc
AR - MAC <t00151@mail.telepac.pt>
2016-08-28 02:53:14 +0100

    timeline 0.7 fixed bcreader

--- www/js/services.js
+++ www/js/services.js
@@ -765,6 +765,14 @@ angular.module('starter.services', [])
         //  return perguntas;
       });
 
+      if (regioes_inicio.length == 0) {
+        console.log("First load regioes inicio");
+        url = "data/regioes_inicio.json";
+        $http.get(url).then(function (response) {
+          console.log("response xxx regios inicio: ", response.data);
+          regioes_inicio = response.data;
+        });
+      }
     };
 
     return {
@@ -851,51 +859,51 @@ angular.module('starter.services', [])
     }
   });
 
-  // .factory('Chats', function () {
-  //   // Might use a resource here that returns a JSON array
-  //
-  //   // Some fake testing data
-  //   var chats = [{
-  //     id: 0,
-  //     name: 'Ben Sparrow',
-  //     lastText: 'You on your way?',
-  //     face: 'img/ben.png'
-  //   }, {
-  //     id: 1,
-  //     name: 'Max Lynx',
-  //     lastText: 'Hey, it\'s me',
-  //     face: 'img/max.png'
-  //   }, {
-  //     id: 2,
-  //     name: 'Adam Bradleyson',
-  //     lastText: 'I should buy a boat',
-  //     face: 'img/adam.jpg'
-  //   }, {
-  //     id: 3,
-  //     name: 'Perry Governor',
-  //     lastText: 'Look at my mukluks!',
-  //     face: 'img/perry.png'
-  //   }, {
-  //     id: 4,
-  //     name: 'Mike Harrington',
-  //     lastText: 'This is wicked good ice cream.',
-  //     face: 'img/mike.png'
-  //   }];
-  //
-  //   return {
-  //     all: function () {
-  //       return chats;
-  //     },
-  //     remove: function (chat) {
-  //       chats.splice(chats.indexOf(chat), 1);
-  //     },
-  //     get: function (chatId) {
-  //       for (var i = 0; i < chats.length; i++) {
-  //         if (chats[i].id === parseInt(chatId)) {
-  //           return chats[i];
-  //         }
-  //       }
-  //       return null;
-  //     }
-  //   };
-  // });
+// .factory('Chats', function () {
+//   // Might use a resource here that returns a JSON array
+//
+//   // Some fake testing data
+//   var chats = [{
+//     id: 0,
+//     name: 'Ben Sparrow',
+//     lastText: 'You on your way?',
+//     face: 'img/ben.png'
+//   }, {
+//     id: 1,
+//     name: 'Max Lynx',
+//     lastText: 'Hey, it\'s me',
+//     face: 'img/max.png'
+//   }, {
+//     id: 2,
+//     name: 'Adam Bradleyson',
+//     lastText: 'I should buy a boat',
+//     face: 'img/adam.jpg'
+//   }, {
+//     id: 3,
+//     name: 'Perry Governor',
+//     lastText: 'Look at my mukluks!',
+//     face: 'img/perry.png'
+//   }, {
+//     id: 4,
+//     name: 'Mike Harrington',
+//     lastText: 'This is wicked good ice cream.',
+//     face: 'img/mike.png'
+//   }];
+//
+//   return {
+//     all: function () {
+//       return chats;
+//     },
+//     remove: function (chat) {
+//       chats.splice(chats.indexOf(chat), 1);
+//     },
+//     get: function (chatId) {
+//       for (var i = 0; i < chats.length; i++) {
+//         if (chats[i].id === parseInt(chatId)) {
+//           return chats[i];
+//         }
+//       }
+//       return null;
+//     }
+//   };
+// });
