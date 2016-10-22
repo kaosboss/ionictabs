7443033f0cd09cda278c6edacd1fb95c1304d24c
AR - MAC <t00151@mail.telepac.pt>
2016-10-22 21:20:09 +0100

    desafios tabela com foto iphone fixes, ios 10

--- www/js/app.js
+++ www/js/app.js
@@ -993,7 +993,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
       // An elaborate, custom popup
       if (!edit)
         caption = $ionicPopup.show({
-          cssClass: "myPopup",
+          cssClass: "myPopupCaption",
           // template: '<textarea ng-model="data.caption" name="Text1" rows="2"></textarea>',
           templateUrl: 'templates/template_caption.html',
           title: 'Album',
@@ -1003,7 +1003,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
             {text: 'Cancelar'},
             {
               text: '<b>Guardar</b>',
-              type: 'button-positive',
+              type: 'button-next-new',
               onTap: function (e) {
                 if (!$scope.data.caption) {
                   e.preventDefault();
@@ -1025,7 +1025,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
           buttons: [
             {
               text: 'Cancelar',
-              type: 'button-small button-positive'
+              type: 'button-small button-clear'
             },
             {
               text: '<b><i class="glyphicon icon ion-trash-a"></i></b>',
@@ -1195,7 +1195,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
           // badgeIconClass : 'ion-checkmark',
           title: title || 'Foto - Album',
           titleContentHtml: '<img class="img-responsive" src="' + thumb + '">',
-          when: when + " na " + $rootScope.currentRI_descricao,
+          when: when + " - " + $rootScope.currentRI_descricao,
           contentHtml: caption
         });
       } else
@@ -1566,13 +1566,14 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
       var fotoDone = function () {
         var found = false;
         var regioes = {};
+        var RI = $regioes.convertRegiaoLongToShort($rootScope.currentRI);
 
         $regioes.getRegioes().then(function (res) {
           regioes = JSON.parse(res || [{}]);
           console.log("fotoCompleto: GOT regioes from cordova service: ", regioes);
           for (var f = 0; f < regioes.length; f++) {
             console.log("f: %s ri: %s r_cur: %s", f, regioes[f].descricao, $rootScope.currentRI);
-            if (regioes[f].descricao == $rootScope.currentRI) {
+            if (regioes[f].nome == RI) {
               if (!regioes[f].fotoDone) {
                 regioes[f].fotoDone = true;
                 $gameFactory.addPoints("foto");
@@ -2296,6 +2297,10 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
 
       };
 
+    $scope.goBack = function () {
+      $ionicHistory.goBack();
+    };
+
       goQR = function (RI) {
         console.log("Go QR", RI);
         // if ($stateParams.RI != "ALL") {
@@ -2720,11 +2725,11 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
               console.log("in circle: %s", aCircles[f].nome);
 
               // if (aCircles[f].locked || !aCircles[f].visited) {
-              if (!aCircles[f].foto || !aCircles[f].visited) {
+              if (!aCircles[f].fotoDone || !aCircles[f].visited) {
                 // if (!aCircles[f].visited) {
                 // aCircles[f].locked = false;
                 aCircles[f].visited = true;
-                aCircles[f].foto = true;
+                aCircles[f].fotoDone = true;
                 $regioes.setRegioes(aCircles);
                 $gameFactory.addPoints("regioes");
                 // createCircles();
@@ -2885,7 +2890,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngSanitize', 'ionic.ion.imageCa
                 // console.warn("Createcircles: regiao not loaded");
                 // }
                 // $scope.regiao.completed = aCircles[i].completed;
-                // $scope.regiao.headeron = $gameFactory.isHeaderOn(reg.nome);
+                $scope.regiao.headeron = $gameFactory.isHeaderOn(reg.nome);
 
                 $timeout(function () {
                   if ((reg.completed) && (!reg.quizDone)) {
